77e40c7ef88cf152a2f2a2905c18e1895defce67
NFish <douxc512@gmail.com>
2025-06-05 18:11:27 +0800

    add missing file

--- api/models/model.py
+++ api/models/model.py
@@ -169,7 +169,6 @@ class App(Base):
     @property
     def deleted_tools(self) -> list:
         from core.tools.tool_manager import ToolManager
-        from services.plugin.plugin_service import PluginService
 
         # get agent mode tools
         app_model_config = self.app_model_config
